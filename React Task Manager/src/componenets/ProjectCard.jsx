3ddf1c69abd9239196bdee15a939f8070a75cff8
kuchbhimingwal <shubhammingi@gmail.com>
2024-03-13 02:17:05 -0400

    styled the main dashboard page and created the make project page

--- React Task Manager/src/componenets/ProjectCard.jsx
+++ React Task Manager/src/componenets/ProjectCard.jsx
@@ -6,11 +6,12 @@ function ProjectCard() {
   const post = useSelector((state)=> state.projectPosts.value.documents)
   console.log(post)
   return (
-    <div className='flex flex-wrap'>
-      <h1>eqdeq</h1>
+    <div className='flex flex-wrap '>
       {post.map((post) => (
-        <div key={post.$id} className='w-1/4 '> 
-          <h3>{post.name}</h3>
+        <div key={post.$id} className='w-1/4 bg-white m-5 rounded-md shadow-md'> 
+          <h3 className='text-left m-2'>{post.name}</h3>
+          <p className='w-auto bg-pink-200 m-8 rounded-md'>{post.discription}</p>
+          <p className='w-auto bg-yellow-200 m-8 rounded-md'>{post.$createdAt}</p>
         </div>
       ))}
     </div>
--- React Task Manager/src/pages/Projects.jsx
+++ React Task Manager/src/pages/Projects.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
+import { useForm } from 'react-hook-form'
+import Input from '../componenets/Input'
 
 function Projects() {
+  const {register, handleSubmit} = useForm();
   return (
-    <div className='bg-TBlueLight h-full rounded-md'>Projects</div>
+    <div className='bg-TBlueLight h-full rounded-md'>
+      <h1 className='text-left m-10 text-xl font-bold leading-tight tracking-tight'>Add Projects</h1>
+      <form action="">
+        <Input 
+          label="Project Name"
+          placeholder="Project Name"
+          type="text"
+          {...register("projectName")}
+        />
+        <Input 
+          label="Deadline Date"
+          placeholder="Deadline Date"
+          type="date"
+          {...register("deadlineDate")}
+        />
+        <Input 
+          label="Project manager name"
+          placeholder="Project manager name"
+          type="text"
+          {...register("managerName")}
+        />
+        <Input 
+          label="Project Deletion Key"
+          placeholder="Project Deletion Key"
+          type="password"
+          {...register("deletionKey")}
+        />
+      </form>
+    </div>
   )
 }
 
